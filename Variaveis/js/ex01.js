61c3439ab370d628241dbac3f20f7c49baf08788
Letícia Ozelin Mavel <007413@libertas.edu.br>
2026-09-03 16:01:55 -0300

    Delete Variaveis directory

--- Variaveis/js/ex01.js
+++
@@ -1,9 +0,0 @@
-const frm = document.querySelector("form");
-const resp = document.querySelector("h3");
-// cria um "ouvinte" de evento, acionado quando o botão submit for clicado.
-
-frm.addEventListener("submit", (e)=> {
-    const nome = frm.inNome.value // obtem valor caixa texto
-    resp.innerText = `Olá ${nome}`
-    e.preventDefault() // evita o envio do form
-})
